d84afd8a4e01dc7698f053e198768fffae33ea12
jmcamacho7 <camachosanchezjm@gmail.com>
2022-01-18 20:32:24 +0100

    Primeros class

--- src/main/java/practica/practica.java
+++ src/main/java/practica/practica.java
@@ -1,4 +1,11 @@
 package practica;
 
-public class practica {
+public class cliente {
+  private int carnetidentidad;
+  private string nombreCliente;
+  private string apellidoCliente;
+  private string direccionCliente;
+  private int telefonocliente;
 }
+
+
---
+++ src/main/java/practica/venta.java
@@ -0,0 +1,10 @@
+package practica;
+
+public class venta {
+    private int idVenta;
+    private string descripcionVenta;
+    private date fechaVenta;
+    private double valorVenta;
+    private double descuentoVenta;
+    private double valorIva;
+}
